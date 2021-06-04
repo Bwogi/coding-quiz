456b4c3f5b9fe21f9d03b10dcc216e20c4ab2d01
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-06-03 18:02:18 -0700

    js code initial element capture

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var startBtn = document.querySelector(".btn");
+var homeSection = document.querySelector(".home");
+var questionSection = document.querySelector(".questions")
+var questionNumber = document.querySelector(".question-number");
+var questionContent = document.querySelector(".question-text");
+var choiceSection = document.querySelector(".choices");
+var f_resultSection = document.querySelector(".f_result");
+var questionLimit = 5; // if you want all questions "quiz.length"
+let questionCounter = 0;
+let currentQuestion;
+let theQuestions = [];
+let theChoices = [];
+let qns_passed = 0;
+let done = 0;
+
 // console.log(startBtn)
 startBtn.addEventListener("click", function() {
+    homeSection.classList.add("hide");
+    questionSection.classList.remove("hide");
+
 
 })
